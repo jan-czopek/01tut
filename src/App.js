762a03459b3fad8bd1916d624a93da66a3839152
Jan Czopek <janczopek@hotmail.com>
2023-09-20 07:47:16 +0200

    add json db

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import apiRequest from "./apiReaques";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
+  const API_URL = 'http://localhost:3500/items';
 
-  const handleCheck = (id) => {
+  // const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
+
+  const [items, setItems] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   localStorage.setItem('shoppingList', JSON.stringify(items));
+  // }, [items]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Nie pobrano danych!');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000)
+  }, []);
+
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id
         ? {
@@ -15,35 +45,62 @@ function App() {
         }
         : item
     );
+    setItems(listItems);
 
-    setAndSaveItems(listItems)
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked })
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems)
+    setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
   };
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem('shoppingList', JSON.stringify(newItems))
-  }
+  const addItemAction = async (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const newItem = { id, checked: false, item };
+    const listItems = [...items, newItem];
+    setItems(listItems);
 
-  const addItemAction = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1
-    const newItem = { id, checked: false, item }
-    const listItems = [...items, newItem]
-    setAndSaveItems(listItems)
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newItem)
+    }
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   }
 
   return (
     <div className="App">
       <Header title="Title" />
-      <Content
-        items={items}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-        addItemAction={addItemAction}
-      />
+      <main>
+        {isLoading && <p>Wczytuję dane...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading &&
+          <Content
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+            addItemAction={addItemAction}
+          />
+        }
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -6,11 +6,11 @@ import { useState } from 'react'
 function Content({ items, handleCheck, handleDelete, addItemAction }) {
   const [search, setSearch] = useState('');
   return (
-    <main>
+    <>
       <AddItem addItemAction={addItemAction} validExpression={/.{3,}/} errorMessage={"At least 3 characters"} />
       <SearchItem search={search} setSearch={setSearch} />
       <ItemList items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} handleCheck={handleCheck} handleDelete={handleDelete} />
-    </main>
+    </>
   );
 }
 
